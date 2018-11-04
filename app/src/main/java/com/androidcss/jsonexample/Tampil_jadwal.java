d7766ca3d540c68b9c00f35a45c11ce00eac0108
Suwardhana <spensajambi@gmail.com>
2018-11-04 21:43:31 +0700

    tambah activity Tampil_jadwal

---
+++ app/src/main/java/com/androidcss/jsonexample/Tampil_jadwal.java
@@ -0,0 +1,13 @@
+package com.androidcss.jsonexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tampil_jadwal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampil_jadwal);
+    }
+}
